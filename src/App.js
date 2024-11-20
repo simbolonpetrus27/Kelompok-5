1697d98ac1ae00e6099f8a078976f573b60dcbb3
intansrg30 <intansoniasiregar@gmail.com>
2024-11-20 10:21:04 +0700

    perbaikan

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/materi" element={<Materi />} />
       </Routes>
     </Router>
