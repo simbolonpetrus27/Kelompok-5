3c25377cd98af9f18bba8fd5913ff8e8550000a4
intansrg30 <intansoniasiregar@gmail.com>
2024-11-20 20:29:36 +0700

    home

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/materi" element={<Materi />} />
         <Route path="/quiz" element={<Quiz />} /> 
       </Routes>
