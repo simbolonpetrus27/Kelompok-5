fe4b6e66854966ab9726f50521144d90f1dc9670
crismansiagian <crismanoliversiagian@gmail.com>
2024-11-22 09:55:41 +0700

    Menambah materi dab style bab 7

--- src/pages/bab7.js
+++ src/pages/bab7.js
@@ -51,29 +51,29 @@ const Bab7 = () => {
       </section>
 
       <section className="bab7-section kata-kunci">
-  <h2 className="section-title">PENJELASAN</h2>
+  <h2 className="section-title">Kata Kunci</h2>
   <ul>
     <li>
-      <strong>Hormon reproduksi</strong>:  Pubertas 
-terjadi sebagai akibat peningkatan sekresi Gonadotropin Releasing 
+      <h2>A.Hormon reproduksi</h2>  
+      <p>Pubertas terjadi sebagai akibat peningkatan sekresi Gonadotropin Releasing 
 Hormone (GnRH) dari hipotalamus, diikuti oleh sekuens perubahan 
 sistem endokrin yang kompleks yang melibatkan sistem umpan balik 
-negatif dan positif (Batubara, 2010).
- Kondisi pubertas pada laki-laki secara hormonal mulai dihasilkan 
+negatif dan positif (Batubara, 2010).</p>
+ <p>Kondisi pubertas pada laki-laki secara hormonal mulai dihasilkan 
 hormon testosteron oleh testis yang bertanggung jawab menyebabkan 
 munculnya ciri-ciri kelamin sekunder. Adapun ciri-ciri kelamin  
 sekunder diantaranya tumbuhnya janggut, suara membesar, 
 tumbuhnya rambut di sekitar alat kelamin dan bentuk badan yang 
 tampak pada seorang laki-laki yang mencapai masa pubertas (masa 
-kematangan fungsi seksual).
- Kondisi pubertas pada perempuan terlihat munculnya ciri
+kematangan fungsi seksual).</p>
+ <p>Kondisi pubertas pada perempuan terlihat munculnya ciri
 ciri kelamin sekunder yang dipengaruhi oleh hormon estrogen 
 dan progesteron di ovarium. Ciri kelamin sekunder perempuan  
 di antaranya adalah payudara membesar, pinggul, tumbuhnya rambut 
 di sekitar alat kelamin, selain itu kedua hormon tersebut berperan 
 dalam proses menstruasi pada saat perempuan mencapai masa 
-pubertas (masa kematangan fungsi seksual).
- Gonad adalah organ yang menghasilkan sel kelamin atau gamet, 
+pubertas (masa kematangan fungsi seksual).</p>
+ <p>Gonad adalah organ yang menghasilkan sel kelamin atau gamet, 
 yaitu spermatozoa (sel sperma/sperma) pada laki-laki, dan ovum 
 (sel telur) pada perempuan. Selain sebagai fungsi reproduksi gonad 
 juga menghasilkan hormon. Ovarium menghasilkan estrogen dan 
@@ -81,11 +81,16 @@ progesteron yang akan bekerjasama dengan FSH dan LH dari hipofisis
 anterior. Hormon-hormon tersebut bertanggung jawab dalam proses 
 menstruasi, kehamilan, fungsi payudara untuk menyusui, pembesaran 
 payudara, pelebaran panggul, dan menjaga karakteristik ciri sekunder 
-perempuan.
- Selama kehamilan ovarium dan plasenta menghasilkan hormon 
+perempuan.Selama kehamilan ovarium dan plasenta menghasilkan hormon 
 relaksin yang akan meningkatkan kelenturan tulang rawan panggul 
 selama kehamilan dan membantu melebarkan leher rahim selama 
-persalinan yang membantu keluarnya bayi dengan mudah. 
+persalinan yang membantu keluarnya bayi dengan mudah.</p>
+<p>Pada laki=laki terdapat testis yang merupakan 
+kelenjar oval terletak di dalam skrotum. Hormon utama yang dihasilkan 
+oleh testis adalah testosteron yang berfungsi mengatur produksi  
+sperma, merangsang perkembangan dan pemeliharaan karakteristik 
+kelamin sekunder laki-laki, seperti pertumbuhan janggut dan 
+membesarnya suara.</p>
       <img
         src="hormon.png"
         alt="Hormon reproduksi"
@@ -93,68 +98,213 @@ persalinan yang membantu keluarnya bayi dengan mudah.
       />
     </li>
     <li>
-      <strong>Kehamilan</strong>: Proses di mana seorang wanita mengandung dan mengembangkan janin di dalam rahimnya.
+      <h2>B.Proses Kehamilan</h2>
+      <h4>A.Fertilisasi/Pembuahan</h4>
+      <p> Fertilisasi atau konsepsi didefInisikan sebagai proses peleburan/
+ penyatuan antara bagian sel sperma dengan satu sel telur (ovum) 
+yang sudah matang. Setelah proses itu akan membentuk zigot yang 
+umumnya terjadi pada sepertiga dari panjang saluran telur atau tuba 
+fallopi. Untuk lebih  jelasnya mari kita lihat gambanya dibawah ini </p> 
       <img
-        src="https://path-to-image/kehamilan.jpg" 
+        src="kehamilan.png" 
         alt="Kehamilan"
         className="kata-kunci-image"
+        
       />
+      <h4>B.Nidasi (Implantasi)</h4>
+      <p>Nidasi atau implantasi adalah peristiwa tertanamnya atau bersarangnya 
+sel telur yang telah dibuahi (fertilized egg) atau hasil konsepsi ke dalam 
+endometrium. Tempat nidasi biasanya pada dinding depan dan dinding 
+belakang di daerah fundus uteri. 
+Setelah implantasi, endometrium disebut desidua. Desidua yang 
+terdapat antar sel telur dan dinding rahim disebu t desidua basalis. </p>
+      <h4>C.Pertumbuhan dan Perkembangan Plasenta</h4>
+      <p> Plasenta adalah bagian kehamilan yang penting. Plasenta memiliki 
+peran sebagai transpor zat dari ibu ke janin, penghasil hormon yang 
+berguna selama kehamilan, serta sebagai barier.</p> 
+<p>Fungsi plasenta adalah mengusahakan janin tumbuh dengan baik. 
+Untuk pertumbuhan ini dibutuhkan adanya penyaluran zat asam, asam 
+amino, vitamin, dan mineral dari ibu ke janin, dan pembuangan CO2
+serta sampah metabolisme janin ke peredaran darah ibu, seperti yang 
+ditampilkan</p>
+<img
+    src="kehamilan2.png" 
+    alt="Kehamilan"
+    className="kata-kunci-image"
+        
+/>
     </li>
     <li>
-      <strong>Menstruasi</strong>: Siklus bulanan yang melibatkan pelepasan sel telur dan darah dari rahim sebagai bagian dari proses reproduksi.
-      <img
-        src="https://path-to-image/menstruasi.jpg" 
+      <h2>C.Proses Menstruasi</h2>
+      <p>Dalam hidup, seorang perempuan akan mengalami menstruasi tidak 
+kurang dari 400 kali, dalam prosesnya akan mengalami pengelupasan 
+dan regenerasi pada endometrium. Darah yang keluar lewat menstruasi 
+seluruhnya tak kurang dari 3 kali jumlah total besi yang ada pada orang 
+dewasa. Pada siklus menstruasi, terdapat fase proliferasi, sekresi, dan 
+menstruasi.</p>
+<h3> a. Fase Proliferasi</h3>
+<p>Pada fase proliferasi, hormon estrogen sangat berpengaruh terhadap 
+perubahan endometrium. Di bawah pengaruh hormon estrogen, 
+endometrium akan mengalami proliferasi (epitel mengalami 
+regenerasi, kelenjar memanjang dan jaringan ikat bertambah padat). 
+Pada masa ini, endometrium tumbuh menjadi tebal kira-kira 3,5 mm. 
+Fase ini berlangsung kira-kira dari hari ke-5 sampai hari ke-14 dari hari 
+pertama haid.</p>
+<h3>b. Fase Sekresi </h3>
+<p> Pada fase ini, hormon yang berpengaruh adalah hormon progesteron. 
+Progesteron menyebabkan keadaan endometrium tetap tebalnya 
+tapi bentuk kelenjar berubah menjadi panjang, membesar, melebar, 
+berkelok-kelok, dan banyak mengeluarkan sekret. Fase sekresi ini 
+berlangsung dari hari ke-14 sampai hari ke-28. Bila tidak terjadi 
+kehamilan maka endometrium akan mengalami peluruhan. </p>
+<h3>c. Fase Menstruasi</h3>
+<p> Menstruasi adalah pendarahan secara periodik dimana darah berasal 
+dari jaringan endometrium yang mati. Menstruasi terjadi sekitar 14 
+hari sesudah ovulasi pada siklus 28 hari. Pada fase menstruasi, terjadi 
+dilatasi atau pelebaran pembuluh darah rahim sehingga bagian-bagian 
+yang mati terlepas berupa darah menstruasi. Sebelum menstruasi 
+berhenti, FSH memacu kembali beberapa folikel untuk tumbuh dan 
+berkembang, dan dengan ini mulai lagi satu siklus menstruasi yang 
+baru. Bila kehamilan tidak terjadi, proses ini berlangsung terus sampai 
+seorang perempuan berumur 45-50 tahun atau sampai usia menopause. </p>
+<img
+    src="mentruasi.png" 
         alt="Menstruasi"
         className="kata-kunci-image"
       />
     </li>
     <li>
-      <strong>Ovarium</strong>: Organ reproduksi wanita yang menghasilkan sel telur dan hormon estrogen serta progesteron.
+      <h2>D.Struktur dan fungsi organ reproduksi perempuan</h2>
+    <img
+        src="wanita.png" 
+        alt="Struktur dan fungsi organ reproduksi perempuan"
+        className="kata-kunci-image"
+      />
+      <strong>1.Ovarium</strong>
+      <p> Ovarium sebagai gonad perempuan berfungsi untukmemproduksi sel telur. Organ ini merupakan sepasang kelenjar 
+yang berbentuk oval terletak di atas rongga pelvis, masing-masing 
+satu buah dekat uterus. Ovarium ini akan selalu dalam posisinya 
+karena diikat oleh ligamen-ligamen dan menempel pada ligamen 
+lebar uterus oleh dua lapisan peritoneum yang melipat di sebut 
+mesovarium (Junquiera, 2018). Pada ovarium terdapat folikel-folikel 
+dan corpus luteum yang berperan dalam menghasilkan hormon 
+progesteron, estrogen, dan relaksin</p> 
+    </li>
+    <li>
+      <strong>2.Oviduk</strong>
+      <p>Bagian selanjutnya yang akan Kalian pelajari adalah saluran 
+telur, di mana apabila sel telur yang dihasilkan oleh ovarium akan 
+diteruskan ke dalam uterus melalui saluran uterin (fallopian) yang 
+sering juga disebut saluran telur (oviduct). Di bagian ujung distal 
+terdapat bagian berbentuk corong yang disebut infundibulum. 
+Pada saluran telur ini terdapat bagian yang paling lebar dan paling 
+panjang disebut ampula, sedangkan bagian yang sempit pendek 
+dan berdinding rapat yang bersatu dengan uterus disebut isthmus. 
+(Moore, 2017).</p>
+    </li>
+    <li>
+      <strong>3.Uterus</strong>
+      <p>Uterus merupakan tempat implatansi sel telur yang telah dibuahi, 
+bahkan perkembangan fetus sampai saatnya dilahirkan juga terjadi 
+di dalam uterus ini. Uterus terletak di antara kantung kemih dan 
+rektum, bentuknya menyerupai buah pir yang terbalik.
+ Secara anatomi uterus dapat dibedakan menjadi tiga bagian yaitu 
+fundus, merupakan bagian yang paling atas, badan yang merupakan 
+bagian utama dari uterus dan cervix yang merupakan bagian paling 
+bawah yang langsung berhubungan dengan vagina (Moore,2017).</p>
+<strong>4.Vagina</strong>
+<p>Vagina merupakan tempat keluarnya aliran haid dan tempat 
+menerima penis selama terjadinya koitus. Vagina merupakan 
+organ yang berotot, berbentuk tabung dan dibatasi oleh selaput 
+mukosa. Panjangnya kira-kira 10 cm, bagian atas vagina secara 
+langsung berhubungan dengan uterus (Moore, 2017). Mukosa 
+vagina banyak mengandung glikogen melalui proses dekomposisi 
+dapat menghasilkan asam-asam organik (Guyton, 2015). Di bagian 
+luar vagina terdapat struktur vulva yang merupakan kumpulan 
+genitalia eksterna perempuan. Bagian-bagian tersebut meliputi 
+Veneris (mons pubis), labium mayora dan minora, klitoris, dan 
+vestibula (Moore, 2017).</p>
+      
+      
       <img
-        src="https://path-to-image/ovarium.jpg" 
-        alt="Ovarium"
+        src="pria.png" 
+        alt="Struktur dan fungsi organ reproduksi laki-laki"
         className="kata-kunci-image"
       />
     </li>
     <li>
-      <strong>Oviduk</strong>: Saluran yang menghubungkan ovarium ke uterus, tempat terjadinya pembuahan sel telur oleh sperma.
+      <h2>E.Struktur dan fungsi organ reproduksi laki-laki</h2>
+    <strong>1.Skrotum</strong>
+    <p>Skrotum merupakan organ genitalia eksterna yang dapat dilihat 
+dari luar, berupa kantung yang disusun oleh kulit tipis dan fascia 
+superficial. Bagian dalam skrotum ini dipisahkan oleh suatu sekat 
+menjadi dua buah kantung yang masing-masing berupa sebuah testis 
+(Moore, 2017). Skrotum juga memiliki otot dartos dan kremaster 
+yang ototnya dapat beradaptasi mengatur temperatur testis ketika 
+panas atau dingin. (Guyton, 2015</p>
+      
+      <strong>2.Testis</strong>
+      <p>Laki-laki memiliki sepasang kelenjar yang berbentuk oval dengan 
+panjang kira-kira 5 cm, diameter 2,5 cm dan berat 10-15 gram. Testis 
+ditutupi oleh lapisan padat dari jaringan fibrosa putih yang disebut 
+tunika albuginea dan juga terdapat gulungan tubulus seminiferus 
+di dalam testis. Bagian inilah yang menghasilkan spermatogenesis 
+(Junquiera, 2018). Untuk lebih jelasnya, struktur testis dapat dilihat 
+pada Gambar dibawah ini</p>
       <img
-        src="https://path-to-image/oviduk.jpg" 
-        alt="Oviduk"
-        className="kata-kunci-image"
-      />
-    </li>
-    <li>
-      <strong>Penis</strong>: Organ reproduksi pria yang berfungsi untuk menyampaikan sperma ke dalam tubuh wanita saat hubungan seksual.
-      <img
-        src="https://path-to-image/penis.jpg" 
-        alt="Penis"
-        className="kata-kunci-image"
-      />
-    </li>
-    <li>
-      <strong>Skrotum</strong>: Kantung kulit yang melindungi dan menahan testis pada pria.
-      <img
-        src="https://path-to-image/skrotum.jpg" 
-        alt="Skrotum"
-        className="kata-kunci-image"
-      />
-    </li>
-    <li>
-      <strong>Testis</strong>: Organ reproduksi pria yang menghasilkan sperma dan hormon testosteron.
-      <img
-        src="https://path-to-image/testis.jpg" 
+        src="pria2.png" 
         alt="Testis"
         className="kata-kunci-image"
       />
     </li>
     <li>
-      <strong>Uterus</strong>: Rahim, tempat berkembangnya janin selama kehamilan.
-      <img
-        src="https://path-to-image/uterus.jpg" 
-        alt="Uterus"
-        className="kata-kunci-image"
-      />
+      <strong>3.Sel Sperma atau spermatozoa</strong>
+      <p> Sekarang bagaimana halnya dengan sel sperma atau spermatozoa? 
+Sel sperma dihasilkan kira-kira 300 juta per hari dan bila telah 
+memasuki alat reproduksi perempuan dapat hidup 18 jam di 
+dalam alat reproduksi perempuan. Sel sperma terdiri atas kepala, 
+bagian tengah dan ekor. Pada bagian kepala terdapat materi inti 
+dan akrosom yang berisi enzim hyaluronidase dan proteinase yang 
+berpengaruh ketika menembus sel telur. Bagian tengah terdapat 
+mitokondria yang berfungsi untuk menghasilkan energi dalam 
+pergerakan. Bagian ekor berbentuk seperti cambuk yang berperan 
+di dalam membantu pergerakan (Junquiera, 2018)</p>
+    </li>
+    <li>
+      <strong>4.Saluran-saluran</strong>
+      <p>Di dalam testis, sel sperma yang telah matang akan bergerak 
+melalui tubulus seminiferus yang bergulung ke saluran yang lurus 
+dan akhirnya ke jaringan tubulus yang disebut rete testis. Sperma 
+ini selanjutnya akan diangkut ke luar testis melalui saluran-saluran 
+tertentu (Sherwood, 2015).</p>
+<p>Adapun saluran dalam testis meliputi beberapa bagian di antaranya 
+adalah epididimis yang berfungsi sebagai tempat pematangan 
+sperma dan menyimpan sperma serta mendorong sperma ke arah 
+uretra selama ejakulasi melalui kontraksi otot polos. Selanjutnya, 
+saluran deferens yang berfungsi menyimpan sperma sampai 
+beberapa bulan dan mendorong sperma ke arah uretra selama 
+ejakulasi. Lalu, saluran ejakulasi yang memiliki panjang kira-kira 
+2 cm dan dibentuk oleh gabungan saluran dari seminal vesikel 
+dan saluran deferens yang berfungsi mendorong sperma ke dalam 
+saluran uretra. Terakhir adalah uretra yang merupakan ujung 
+saluran dari sistem reproduksi laki-laki yang berfungsi sebagai 
+saluran sperma dan urin (Moore, 2017)</p>
+<strong> 5.Kelenjar-kelenjar tambahan</strong>
+<p> Sekarang Kalian akan membahas kelenjar-kelenjar tambahan yang 
+berfungsi menyekresikan cairan sperma. Misalnya saja kantong 
+sperma (seminal vesicle) berfungsi menyekresikan cairan kental 
+yang bersifat alkalis dan kaya akan fruktosa. Kelenjar prostat 
+menghasilkan cairan alkalis untuk menetralisasi asam di dalam 
+uretra dan vagina. Kelenjar bulbourethral merupakan kelenjar 
+tambahan lainnya, berfungsi menghasilkan lendir dan zat yang 
+bersifat untuk menetralisasi urin (Sherwood, 2016).</p>
+<strong>6.Penis</strong>
+<p>Penis digunakan untuk mentransfer sperma ke dalam vagina. Ujung 
+distal penis membesar disebut glans, bagian ini ditutupi oleh kulit 
+yang terpisah dinamakan prepuse. Secara internal penis disusun 
+oleh tiga jaringan masa silindris yang diikat bersama-sama oleh 
+jaringan fibrosa. Ketiga jaringan tersebut mengandung banyak 
+sinus-sinus pembuluh darah.</p>
     </li>
   </ul>
 </section>
