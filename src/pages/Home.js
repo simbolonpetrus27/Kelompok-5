8a90b64a5e6d5ce6a8625a91fee0af5b28e492a8
crismansiagian <crismanoliversiagian@gmail.com>
2024-11-26 10:39:30 +0700

    Mengubah style home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,15 +19,12 @@ function Home() {
   return (
     <div className="home">
       <div className="hero-section">
-        <div className="hero-content">
-          <h1>BIONIK</h1>
-          <p>Website yang membantu kamu di Pelajaran Biologi Kelas XI</p>
           <div className="search-box">
             <button onClick={handleExploreClick}>Explore Materi</button>
           </div>
         </div>
       </div>
-    </div>
+    
   );
 }
 
