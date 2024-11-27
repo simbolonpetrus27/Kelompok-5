96f1e07d4f7b69f88b014276147d6aebf79db1c0
GracesyaAngelinaSimanjuntak <gracesimanjuntak57@gmail.com>
2024-11-27 07:23:31 +0700

    menambah_materi_bab2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Materi from './pages/Materi';
 import Quiz from './pages/Quiz';
 import Bab1 from './pages/bab1';
+import Bab2 from './pages/bab2';
 import Bab4 from './pages/bab4';
 import Bab5 from './pages/bab5';
 import Bab6 from './pages/bab6';
@@ -23,6 +24,7 @@ function App() {
         <Route path="/materi" element={<Materi />} />
         <Route path="/quiz" element={<Quiz />} /> 
         <Route path="/bab1" element={<Bab1 />} />
+        <Route path="/bab2" element={<Bab2 />} />
         <Route path="/bab4" element={<Bab4 />} />
         <Route path="/bab5" element={<Bab5 />} />
         <Route path="/bab6" element={<Bab6 />} />
